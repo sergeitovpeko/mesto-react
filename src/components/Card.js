c3d2fce7cdb7f3b9546934ba33bd5332d1531e23
sergeitovpeko <sergei.tovpeko@yandex.ru>
2023-06-30 16:39:45 +0300

    ПР 12. Отправка на 2 ревью

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,53 @@
-import React from "react"
+import { useContext } from "react"
+import CurrentUserContext from "../contexts/CurrentUserContext"
 
-function Card(card) {
+function Card(props) {
+  const currentUser = useContext(CurrentUserContext)
+  const isLiked = props.card.likes.some((user) => user._id === currentUser._id)
+  const likeButtonClassName = `elements__button elements__button_like ${
+    isLiked ? "elements__button_like_active" : ""
+  }`
+  const isOwner = props.card.owner._id === currentUser._id
+
+  function handleLikeClick() {
+    props.onCardLike(props.card)
+  }
+
   function handleCardClick() {
-    card.onCardClick(card)
+    props.onCardClick(props.card)
+  }
+
+  function handleDeleteClick() {
+    props.onCardDelete(props.card)
+    props.onConfirmationPopup(true)
   }
 
   return (
     <div className="elements__list-item">
-      <button
-        className="elements__button elements__button_delete"
-        type="button"
-      ></button>
+      {isOwner && (
+        <button
+          className="elements__button elements__button_delete"
+          onClick={handleDeleteClick}
+          type="button"
+        />
+      )}
+
       <img
         className="elements__image"
-        src={card.link}
-        alt={card.name}
+        src={props.card.link}
+        alt={props.card.name}
         onClick={handleCardClick}
       />
       <div className="elements__info">
-        <h2 className="elements__title">{card.name}</h2>
+        <h2 className="elements__title">{props.card.name}</h2>
         <div className="elements__like">
           <button
-            className="elements__button elements__button_like"
+            className={likeButtonClassName}
+            onClick={handleLikeClick}
             type="button"
           ></button>
-          <p className="elements__likes-quantity">{card.likes.length}</p>
+
+          <p className="elements__likes-quantity">{props.card.likes.length}</p>
         </div>
       </div>
     </div>
